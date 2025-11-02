e07b383097a385b16c51e2738aedbdc626ae222b
Epreen Njolomole <epreennjolomole451@gmail.com>
2025-11-02 07:25:28 +0200

    chore: init Next.js web foundation

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -6,7 +6,7 @@ import { gsap } from 'gsap'
 export default function HomeHero() {
     return (
         <motion.div
-            className="flex items-center justify-center h-screen bg-brand font-light font-sans"
+            className="flex items-center justify-center h-screen bg-brand"
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
