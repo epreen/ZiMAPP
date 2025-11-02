e11117f65c93b650cadb4b5229612e74084f3270
Epreen Njolomole <125865502+epreen@users.noreply.github.com>
2025-11-02 06:25:45 +0200

    Delete packages/ui directory

--- packages/ui/index.tsx
+++
@@ -1,5 +0,0 @@
-export const Button = ({ children }: { children: React.ReactNode }) => (
-    <button className="px-4 py-2 bg-purple-700 text-white rounded-xl">
-        {children}
-    </button>
-);
