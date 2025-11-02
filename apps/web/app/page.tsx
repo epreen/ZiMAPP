84824362921a9731077c4b39291e274e1f15bb23
Epreen Njolomole <125865502+epreen@users.noreply.github.com>
2025-11-02 06:25:04 +0200

    Delete apps directory

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@zimapp/ui";
-
-export default function Home() {
-    return (
-        <main className="flex flex-col items-center justify-center h-screen">
-            <Button>ZiMAPP Ready!</Button>
-        </main>
-    );
-}
